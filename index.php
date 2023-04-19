d47ef379180326bd522dee1b548279f8310e0c88
silvialep <silvia.lepore_sl@libero.it>
2023-04-19 15:39:30 +0200

    feat: created + imported functions.php

--- index.php
+++ index.php
@@ -1,25 +1,7 @@
 <?php
 
 
-$passwordLength = $_GET['passlen'] ?? 'false';
-
-// var_dump($_GET['passlen']);
-
-function randomPassword($passwordLength) {
-      $rpsw = '';
-      $source = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890|@#~$%()=^*+[]{}-_';
-      
-      for($i = 1; $i <= $passwordLength; $i++) {
-        //Generate a random index based on the string in question.
-        $randomIndex = mt_rand(0, strlen($source) - 1);
-
-        //Print out the random character.
-        $newChar = $source[$randomIndex];
-
-        $rpsw .= $newChar;
-      }
-      return $rpsw;
-  }
+include './partials/functions.php';
 
 ?>
 
@@ -50,10 +32,6 @@ function randomPassword($passwordLength) {
         </div>
 
         <?php
-
-        
-
-        
     }
     ?>
     
@@ -62,11 +40,5 @@ function randomPassword($passwordLength) {
 </html>
 
 <!-- 
-Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
-Milestone 1
-Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
-Scriviamo tutto (logica e layout) in un unico file index.php
-Milestone 2
-Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
 Milestone 3 (BONUS)
 Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente. -->
---
+++ partials/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$passwordLength = $_GET['passlen'] ?? 'false';
+
+// var_dump($_GET['passlen']);
+
+function randomPassword($passwordLength) {
+      $rpsw = '';
+      $source = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890|@#~$%()=^*+[]{}-_';
+      
+      for($i = 1; $i <= $passwordLength; $i++) {
+        //Generate a random index based on the string in question.
+        $randomIndex = mt_rand(0, strlen($source) - 1);
+
+        //Print out the random character.
+        $newChar = $source[$randomIndex];
+
+        $rpsw .= $newChar;
+      }
+      return $rpsw;
+  }
